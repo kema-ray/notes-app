5e8ea26477ed06b0d44f33ada007c9e4dc561987
kema-ray <rachel.oyondi@roamtech.com>
2025-03-06 17:19:47 +0300

    created a user interface got the index page

--- app.vue
+++ app.vue
@@ -5,3 +5,17 @@
     </NuxtLayout>
   </div>
 </template>
+
+<style>
+.font-playfair {
+  font-family: 'Playfair', serif;
+  font-optical-sizing: auto;
+  font-weight: 400;
+  font-style: normal;
+  font-variation-settings: 'wdth' 100;
+}
+
+* {
+  font-family: 'Inter', serif;
+}
+</style>
